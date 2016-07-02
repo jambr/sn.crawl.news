0661a7f99c4602c6825283d8827b013c3b7d4936
Karl Stoney <kstoney@thoughtworks.com>
2016-07-02 19:47:42 +0100

    Added sentiment to the startup script

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ let KeyValueStore = require('sn.core').Default.Store;
 let MessageBridge = require('./lib/messageBridge');
 let NewsGrabber = require('./lib/newsGrabber');
 let NewsOutlet = require('./lib/news/google');
+let Sentiment = require('./lib/sentiment');
 
 let broker = new Broker('sn:topic');
 let symbolStore = new KeyValueStore('sn:crawl:news:symbols');
 let publishedNewsStore = new KeyValueStore('sn:crawl:news:publishedNews');
 let newsOutlet = new NewsOutlet();
-let newsGrabber = new NewsGrabber(symbolStore, newsOutlet, publishedNewsStore);
+let sentiment = new Sentiment();
+let newsGrabber = new NewsGrabber(symbolStore, newsOutlet, publishedNewsStore, sentiment);
  
 let messageBridge = new MessageBridge(broker, newsGrabber);
 messageBridge.start(() => {
